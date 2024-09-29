774066dc02d2300a2411eb64439265aff1bcba32
sumit1414 <sumitjaiswal315@gmail.com>
2024-09-29 16:50:26 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import Header from './components/Header';
-import Banner from './components/Banner';
-import Features from './components/Features';
-import Products from './components/Products';
-import Categories from './components/Categories';
-import Reviews from './components/Reviews';
-import Blogs from './components/Blogs';
-import Footer from './components/Footer';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Banner from "./components/Banner";
+import Features from "./components/Features";
+import Products from "./components/Products";
+import Categories from "./components/Categories";
+import Reviews from "./components/Reviews";
+import Blogs from "./components/Blogs";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
   return (
