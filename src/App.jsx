211c8d45a67020e162743002b7f6fbea35bf8848
sumit1414 <sumitjaiswal315@gmail.com>
2024-09-29 17:27:57 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,18 @@
 import React from "react";
-import Header from "./components/Header";
-import Banner from "./components/Banner";
-import Features from "./components/Features";
-import Products from "./components/Products";
-import Categories from "./components/Categories";
-import Reviews from "./components/Reviews";
-import Blogs from "./components/Blogs";
-import Footer from "./components/Footer";
 import "./App.css";
+import Home from "./Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="app mt-set">
-      <Header />
-      <Banner />
-      <Features />
-      <Products />
-      <Categories />
-      <Reviews />
-      <Blogs />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Home" element={<Home />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
